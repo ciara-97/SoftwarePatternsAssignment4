84a9b0ef0a12601f37a955856ea04cf6678b39ae
Ciara <C15373956@mydit.ie>
2019-04-16 17:36:44 +0100

    Initial commit with customer login and register

---
+++ src/main/java/com/example/demo/item/StockItemService.java
@@ -0,0 +1,49 @@
+package com.example.demo.item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StockItemService {
+
+	@Autowired
+	private StockItemRepository stockItemRepository;
+		
+	public List<StockItem> getAllItems(){
+		List<StockItem> items = new ArrayList<StockItem>();
+		for(StockItem stock: stockItemRepository.findAll()) {
+			items.add(stock);
+		}
+		return items;
+	}
+	
+	public StockItem getItemById(int id) {
+		return stockItemRepository.findOne(id);
+	}
+	
+	/*public Customer getUserByUsernameAndPassword(String username, String password) {
+		Customer c = customerRepository.findByUsernameAndPassword(username, password);
+		return c;
+	}*/
+	
+	/*public Customer getUserByUsername(String username) {
+		Customer c = customerRepository.findUserByUsername(username);
+		return c;
+	}*/
+	
+	public void addItem(StockItem item) {
+		stockItemRepository.save(item);
+	}
+	
+	public void updateItem(int id, StockItem item) {
+		stockItemRepository.save(item);
+	}
+	
+	public void deleteItem(int id) {
+		stockItemRepository.delete(id);
+	}
+
+}
